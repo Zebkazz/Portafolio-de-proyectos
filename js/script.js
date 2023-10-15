e6d62c6dd66ae764ccc61cfc04d1ffc3f7c60019
Zebkazz Castle <zebkazz01@gmail.com>
2023-10-14 21:39:54 -0500

    Portafolio de proyectos Actualizacion de estilos e implementacion de jquery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+$(function() {
+  var $body = $(document);
+  $body.bind('scroll', function() {
+    // "Disable" the horizontal scroll.
+    if ($body.scrollLeft() !== 0) {
+      $body.scrollLeft(0);
+    }
+  });
+});
+
+
+
 // mesaje de bienvenida
-function mensaje(){
-  alert("Por favor presiona f11 para una mayor inmersion");
-}
+  function mensaje(){
+    alert("Por favor presiona f11 para una mayor inmersion");
+  }
 
 document.querySelector(".contmenu").style.transition = "all 1500ms";
 document.querySelector(".contmenu").style.left= "0px";
